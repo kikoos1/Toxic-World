09756abbf88042480d22e250e7d7e36353a57c7c
kikoos1 <kikoos1@abv.bg>
2016-11-10 21:44:07 +0200

    Add files via upload

---
+++ Healthsystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Healthsystem : MonoBehaviour {
+    public float health;
+    public float curhealth;
+    Slider healthbar;
+    public bool alive;
+
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+        curhealth = health;
+        alive = true;
+        healthbar.value = curhealth;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+   public void TakeDamage(float amount)
+    {
+        if (!alive)
+        {
+            return;
+        }
+        if (curhealth <= 0)
+        {
+            curhealth = 0;
+            alive = false;
+            gameObject.SetActive(false);
+            healthbar.gameObject.SetActive(false);
+        }
+        curhealth -= amount;
+        healthbar.value = curhealth;
+    }
+   public void Healing(float healing)
+    {
+        if (curhealth <= 100)
+        {
+            curhealth += healing;
+        }
+        healthbar.value = curhealth;
+    }
+}
---
+++ PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    Rigidbody2D rb;
+    public float speed;
+    public float jump;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f,1f,1f);
+        }else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f,1f,1f);
+        }
+        
+	
+	}
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, jump);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        
+    }
+}
